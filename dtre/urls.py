f6effbb27b142ddcf78c5886500af9d2f4a19cd7
Judgewell Ephraim <ephraimjudgewell@gmail.com>
2020-10-16 03:58:03 +0100

    updated static url for uwsgi to find them

--- dtre/urls.py
+++ dtre/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
